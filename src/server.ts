824b4618b68bcfba9e6ee843fef61aaa00c0da38
Elwin Dela Cruz <somi-elwin@somi-elwins-MacBook-Pro.local>
2017-06-30 18:00:42 +0800

    change base url

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,22 @@
-import 'reflect-metadata';
-import 'zone.js/dist/zone-node';
-import { platformServer, renderModuleFactory } from '@angular/platform-server'
-import { enableProdMode } from '@angular/core'
-import { AppServerModuleNgFactory } from '../dist/ngfactory/src/app/app.server.module.ngfactory'
-import * as express from 'express';
-import { readFileSync } from 'fs';
-import { join } from 'path';
-
-const PORT = 4000;
-
-enableProdMode();
-
+// server.js
+const express = require('express');
 const app = express();
-
-let template = readFileSync(join(__dirname, '..', 'dist', 'index.html')).toString();
-
-app.engine('html', (_, options, callback) => {
-  const opts = { document: template, url: options.req.url };
-
-  renderModuleFactory(AppServerModuleNgFactory, opts)
-    .then(html => callback(null, html));
-});
-
-app.set('view engine', 'html');
-app.set('views', 'src')
-
-app.get('*.*', express.static(join(__dirname, '..', 'dist')));
-
-app.get('*', (req, res) => {
-  res.render('index', { req });
-});
-
-app.listen(PORT, () => {
-  console.log(`listening on http://localhost:${PORT}!`);
-});
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+// ...
