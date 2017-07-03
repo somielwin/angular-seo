3d36ef4740859561b261e59fd1b9daa00015f3bd
Elwin Dela Cruz <somi-elwin@somi-elwins-MacBook-Pro.local>
2017-07-03 16:04:19 +0800

    my first angular project --prod

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var app = express();
 const port = process.env.PORT || '5000';
 
 app.set('port', port);
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/[^\.]+$', function(req, res) {
 	res.set('Content-Type', 'text/html')
-		.sendFile(path.join(__dirname, '/dist/index.html'))
+		.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.listen(app.get('port'), function(){
